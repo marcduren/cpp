c86b151ae28a4c868054501c1a269abca8851394
Marc DUREN <marc.duren@mdsp.fr>
2023-05-21 22:58:59 +0200

    ajout splitString

--- strings.cpp
+++ strings.cpp
@@ -19,6 +19,24 @@ string ltrim(const string& str) {
     }
     return str.substr(first);
 }
+std::vector<std::string> splitString(std::string str, char splitter) {
+    std::vector<std::string> result;
+    std::string current = "";
+    for (int i = 0; i < str.size(); i++) {
+        if (str[i] == splitter) {
+            if (current != "") {
+                result.push_back(current);
+                current = "";
+            }
+            continue;
+        }
+        current += str[i];
+    }
+    if (current.size() != 0)
+        result.push_back(current);
+    return result;
+}
+
 std::string to_uppercase(const std::string& str) {
     std::string result = "";
     for (char c : str) {
