7a9ad06f256edeeace6e0db4068e4e2e251aff76
Marc DUREN <marc.duren@mdsp.fr>
2023-06-10 17:23:30 +0200

    ajout fonctions Explode, ReplaceString

--- strings.cpp
+++ strings.cpp
@@ -36,6 +36,32 @@ std::vector<std::string> splitString(std::string str, char splitter) {
         result.push_back(current);
     return result;
 }
+#include <vector>
+#include <string>
+#include <sstream>
+#include "stringFonctions.h"
+
+std::vector<std::string> Explode(std::string const & s, char delim) {
+    std::vector<std::string> result;
+    std::istringstream iss(s);
+
+    for (std::string token; std::getline(iss, token, delim);) {
+        result.push_back(std::move(token));
+    }
+
+    return result;
+}
+
+std::string ReplaceString(std::string subject, const std::string& search,
+                          const std::string& replace) {
+    size_t pos = 0;
+    while ((pos = subject.find(search, pos)) != std::string::npos) {
+         subject.replace(pos, search.length(), replace);
+         pos += replace.length();
+    }
+    return subject;
+}
+
 
 std::string to_uppercase(const std::string& str) {
     std::string result = "";
